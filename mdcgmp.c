23d415bb13bc067b3c03c0b30561ea1ac763febc
Raian Pierre <54862169+raianffc@users.noreply.github.com>
2024-06-11 17:01:14 -0300

    Update mdcgmp.c

--- mdcgmp.c
+++ mdcgmp.c
@@ -10,8 +10,8 @@
 #include <stdlib.h>
 #include <gmp.h>
 
-unsigned int mdc(unsigned int num1, unsigned int num2) {
-    unsigned int resto;
+int mdc(int num1, int num2) {
+    int resto;
     do {
         resto = num1 % num2;
         num1 = num2;
@@ -20,130 +20,137 @@ unsigned int mdc(unsigned int num1, unsigned int num2) {
     return num1;
 }
 
-unsigned int mmc(unsigned int num1, unsigned int num2) {
-    unsigned int resto, a;
+int mmc(int num1, int num2) {
+    int resto, a;
     if(num2==0) return num1;
     a = mdc(num1,num2);
     return (num1 * num2) / a;
     
-}
-
-unsigned long* FracCont(unsigned long x, unsigned long q, unsigned long N, int *tamL) {
+}mpz_t *FracCont(int *tamL, double x, int q, int N) {
     int tam = 1;
-    double x_inic = (double)x;
-    double x_double = (double)x;
-    unsigned long *L;
-    L = (unsigned long*)malloc(tam * sizeof(unsigned long));
+    double x_inic = x;
+    mpz_t *L;
+
+    // Alocação inicial
+    L = (mpz_t *)malloc(tam * sizeof(mpz_t));
     if (L == NULL) {
-        printf("Erro na alocacao de memoria.");
+        printf("Erro na alocacao de memoria.\n");
         exit(1);
     }
+
+    mpz_init_set_ui(L[0], 0); // Inicializar a primeira posição de L
+
     if (x == 1) {
         tam = 2;
-        L = (unsigned long*)realloc(L, tam * sizeof(unsigned long));
+        L = (mpz_t *)realloc(L, tam * sizeof(mpz_t));
         if (L == NULL) {
-            printf("Erro na alocacao de memoria.");
+            printf("Erro na alocacao de memoria.\n");
             exit(1);
         }
-        L[0] = 1;
-        L[1] = 0;
+        mpz_init_set_ui(L[0], 1);
+        mpz_init_set_ui(L[1], 0);
         x = 0;
     } else {
-        L[0] = -1;
+        mpz_set_si(L[0], -1);
     }
-    x_double = x_double / (double)q;
+
+    x = x / q;
     int i = 0;
     double max = log(x_inic) / log(1.6);
+
     do {
-        int c = (int) x_double;
-        if (tam != 1) {
-            L = (unsigned long*)realloc(L, (tam + 1) * sizeof(unsigned long));
-            if (L == NULL) {
-                printf("Erro na alocacao de memoria.");
-                exit(1);
-            }
-            L[tam] = (unsigned long) c;
-        } else {
-            L[0] = (unsigned long) c;
+        int c = (int)x;
+        L = (mpz_t *)realloc(L, (tam + 1) * sizeof(mpz_t));
+        if (L == NULL) {
+            printf("Erro na alocacao de memoria.\n");
+            exit(1);
         }
-        x_double = x_double - (double)c;
-        if (x_double >= 0) {
-            x_double = 1 / x_double;
-            if (x_double > x_inic || x_double > N) {
-                x_double = 0;
+        mpz_init_set_ui(L[tam], c);
+
+        x = x - c;
+        if (x >= 0.00001) {  // Use um epsilon pequeno para evitar erros de precisão
+            x = 1 / x;
+            if (x > x_inic || x > N) {
+                x = 0;
             }
         } else {
-            x_double = 0;
+            x = 0;
         }
         i++;
         tam++;
-    } while (x_double > 0 && i < max);
-    *tamL = tam - 1;
+    } while (x > 0 && i < max);
+
+    *tamL = tam;
+
+    printf("tamanho de L = %d\ntamanho de tam: %d\n", *tamL, tam);
+    for (int i = 0; i < tam; i++) {
+        gmp_printf("L[%d]: %Zd\n", i, L[i]);
+    }
+
     return L;
 }
 
-unsigned long** Frac(unsigned long *L, int *tamL) {
+
+// Função Frac usando GMP
+mpz_t **Frac(mpz_t *L, int *tamL) {
+    mpz_t **F;
     int tamF = *tamL;
+
     if (*tamL == 0) {
         *tamL = 1;
-        L = (unsigned long*)realloc(L, (*tamL) * sizeof(unsigned long));
+        mpz_realloc2(*L, (*tamL) * sizeof(mpz_t));
         if (L == NULL) {
-            printf("Erro na alocacao de memoria.");
+            printf("Erro na alocacao de memoria.\n");
             exit(1);
         }
-        L[0] = 1;
+        mpz_init_set_ui(L[0], 1);
     }
-    unsigned long **F = (unsigned long**)malloc(2 * sizeof(unsigned long *));
-    if (F == NULL) {
-        printf("Erro na alocacao de memoria.");
-        exit(1);
-    }
-    for (int i = 0; i < 2; i++) {
-        F[i] = (unsigned long*)malloc((tamF) * sizeof(unsigned long));
-        if (F[i] == NULL) {
-            printf("\nErro na alocacao de memoria.\n");
-            exit(1);
-        }
-    }
-    for(int i = 0;i<2; i++){
-        for(int j = 0; j<(tamF);j++){
-            F[i][j]=0.0;
-        }
-    }
-    int num = 1;
-    int den = 1;
-    for ( int i=0; i<((tamF)-1);i++){
-        num = 1;
-        den = (int)L[(tamF)-i-1];
-        for (int j=((tamF)-i-2); j>0;j--){
-            int temp=num;
-            num = den;
-            den = L[j]*den+temp;
-        }
-        F[0][i+1]=(unsigned long)num;
-        F[1][i+1]=(unsigned long)den;
-    }
-    F[0][0]=(tamF)-1;
-    printf("print F[0][0]: %lu", F[0][0]);
 
-    int total = (int)F[1][2];
-    for(int j=3;j<tamF;j++){
-        total = mmc(total,(int)F[1][j]);
+    F = (mpz_t **)malloc(2 * sizeof(mpz_t *));
+    for (int i = 0; i < 2; i++) {
+        F[i] = (mpz_t *)malloc(tamF * sizeof(mpz_t));
+        for (int j = 0; j < tamF; j++) {
+            mpz_init(F[i][j]);
+        }
     }
-    F[1][0]=(unsigned long)total;
-    if(F[1][0]==0 && *tamL==1){
-        printf("\nFracao inexistente: %lu \n", L[0]);
+
+    mpz_t num, den, temp;
+    mpz_init(num);
+    mpz_init(den);
+    mpz_init(temp);
+
+    for (int i = 0; i < tamF; i++) {
+        mpz_set_ui(num, 1);
+        mpz_set(den, L[tamF - i - 1]);
+        for (int j = tamF - i - 2; j >= 0; j--) {
+            mpz_set(temp, num);
+            mpz_set(num, den);
+            mpz_mul(den, L[j], den);
+            mpz_add(den, den, temp);
+        }
+        mpz_set(F[0][i], num);
+        mpz_set(F[1][i], den);
     }
-    free(L);
+
+    mpz_t total;
+    mpz_init(total);
+    mpz_set(total, F[1][0]);
+
+    for (int j = 1; j < tamF; j++) {
+        mpz_lcm(total, total, F[1][j]);
+    }
+
+    mpz_set(F[1][0], total);
+
+    mpz_clear(num);
+    mpz_clear(den);
+    mpz_clear(temp);
+    mpz_clear(total);
+
     return F;
 }
 
-// EstimaOrdem usando GMP
-#include <stdio.h>
-#include <stdlib.h>
-#include <gmp.h>
-
-mpz_t **EstimaOrdem(unsigned long r, unsigned long *result, unsigned long q, unsigned long N, int n) {
+mpz_t **EstimaOrdem(unsigned long r, double *result, unsigned long q, unsigned long N, int n) {
     mpz_t **R;
 
     // Alocar memória para R usando GMP
@@ -165,35 +172,44 @@ mpz_t **EstimaOrdem(unsigned long r, unsigned long *result, unsigned long q, uns
     }
 
     printf("\nTentando estimar a ordem r=ord(x,N) ou múltiplo ou divisor dela para extrair os fatores de N\n");
+
     for (int i = 0; i < n; i++) {
         for (int j = -1; j <= 1; j++) {
             printf(".");
 
             // Chamar FracCont com os tipos originais (unsigned long)
             int taml = 0;
-            unsigned long adjusted_result = result[i] + j; // Ajustando resultado
-            unsigned long *frac_cont_result = FracCont(adjusted_result, q, N, &taml);
+            double adjusted_result = result[i] + j; // Ajustando resultado
+            mpz_t *frac_cont_result = FracCont(&taml, adjusted_result, q, N);
 
             // Chamar Frac com os tipos originais (unsigned long)
-            unsigned long **t = Frac(frac_cont_result, &taml);
+            mpz_t **t = Frac(frac_cont_result, &taml);
+
+            gmp_printf("t[1][0]: %Zd\n", t[1][0]);
 
             // Converter o resultado de Frac para mpz_t para armazenar em R
-            mpz_set_ui(R[i][j + 1], t[1][0]);
+            mpz_set(R[i][j + 1], t[1][0]);
 
-            // Liberação da memória
-            for (int k = 0; k < 2; k++) {
-                free(t[k]); // Corrigido para garantir a liberação correta
+            // Liberação da memória para frac_cont_result
+            for (int k = 0; k < taml; k++) {
+                mpz_clear(frac_cont_result[k]);
             }
-            free(t); // Certifique-se de que não está liberando duas vezes
             free(frac_cont_result);
+
+            // Liberação da memória para t
+            for (int k = 0; k < 2; k++) {
+                for (int l = 0; l < taml; l++) {
+                    mpz_clear(t[k][l]);
+                }
+                free(t[k]);
+            }
+            free(t);
         }
         printf("\n");
     }
 
     return R;
 }
-
-
 
 int main(){
     unsigned long p1 = 37;
@@ -208,8 +224,9 @@ int main(){
     int tamSoma_P;
     unsigned long *Z; 
     mpz_t **R;
-    unsigned long result [15]= {343700, 774782, 570891, 367001, 93206, 1042750, 425256, 1, 757304, 1042750, 343700, 1042750, 297096, 75730, 413604};
+    double result [15]= {343700, 774782, 570891, 367001, 93206, 1042750, 425256, 1, 757304, 1042750, 343700, 1042750, 297096, 75730, 413604};
     R = EstimaOrdem(r, result, q, N, n);
+    
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < 3; j++) {
             gmp_printf("R[%d][%d]: %Zd\n", i, j, R[i][j]);
